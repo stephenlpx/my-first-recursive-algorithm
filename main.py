03b9caf3b0b10fd502a61518146186cfa1b516c7
Stephen <slpotgieter1@gmail.com>
2022-07-13 18:36:25 +0100

    commit - 2 (16-28)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#recursive method
+
 def get_factorial(n):
     #below are the base cases which is used to terminate the recursive algorithm
     if n == 1:
@@ -6,7 +8,21 @@ def get_factorial(n):
         return 1
     else:
         #if the number does not reach the target, then repeat and store the value until the target is hit
-        #then return the final value 
+        #then return the final value
         return n * get_factorial(n - 1)
 
 print(get_factorial(6))
+
+#iterative method
+def get_fac(n):
+    if n == 1:
+        return 1
+    elif n == 0:
+        return 1
+    else:
+        fact = 1
+        for x in range(1, n+1):
+            fact = fact*x
+        return fact
+
+print(get_fac(6))
